fb6411714af864c01502534362f68b22b4241b21
Renato Spacek <47311498+renatospacek@users.noreply.github.com>
2019-03-05 06:34:02 -0500

    Update input.jl

--- input.jl
+++ input.jl
@@ -6,62 +6,53 @@ using LinearAlgebra
 # ==============================================================================
 #                           Simulation parameters
 # ==============================================================================
-const t 	        = 10.0                               
-const N 	        = 5*10^4                   
+const t 	    = 10.0                               
+const N 	    = 5*10^4                   
 const dt            = t/N                                  
-const dim	        = 3                                   
+const dim	    = 3                                   
 const ndim          = 10                                  
 const nPart         = ndim^dim                                                
-const γ 	        = 1.0                                  
-const β 	        = 1.0                                  
+const γ 	    = 1.0                                  
+const β 	    = 1.0                                  
 const σ	            = 1.0                                 
 const ε	            = 1.0   
 const nopot         = 1                           
-const rc	        = 2^(1/6)*σ                           
+const rc	    = 2^(1/6)*σ                           
 const R             = 1        
 const outFreq       = 1000
 const η             = 0.5
 const a             = 1.0
 const Σ             = sqrt(2*γ/β)
- S             = a*ndim*Array{Float64}(I, 3, 3)
- Sinv          = inv(S) 
- A             = η*[1.0 0 0; 0 -1/2 0; 0 0 -1/2]
-#integrator          = "EM"
+S            	    = a*ndim*Array{Float64}(I, 3, 3)
+Sinv                = inv(S) 
+A                   = η*[1.0 0 0; 0 -1/2 0; 0 0 -1/2]
+
+#integrator         = "EM"
+#integ = Symbol(integrator)
 
 const tfac = 2/3/nPart
 const vol = a^3*nPart
 const Mmax = floor(Int, a/rc*ndim)
 
-    
-    
-#integ = Symbol(integrator)
 
 # ==============================================================================
-#                         Base/Type definitions
+#                         Function definitions
 # ==============================================================================
 
-Base.show(io::IO, f::Float64) = @printf(io, "%1.3f", f)
+Base.show(io::IO, f::Float64) = @printf(io, "%1.2f", f)
+
 
 function Base.exp(A::Array{T,1}) where T<:Real
     return exp(Diagonal(A))
 end
 
-#eye(T::Type, N::Int) = Array{T,2}(I, N, N)
-#eye(N::Int) = Array{Float64,2}(I, N, N)
 
+eye(T::Type, N::Int) = Array{T,2}(I, N, N)
+eye(N::Int) = Array{Float64,2}(I, N, N)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# ==============================================================================
+#                         Type definitions
+# ==============================================================================
 
 mutable struct particle{T<:AbstractFloat}
 	q::Array{T,2}
